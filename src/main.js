597cfb0c0e8a2b1227c754c6d20d7f967dbab8e4
apem5186 <apem5186@naver.com>
2024-08-17 01:23:37 +0900

    #4 comment 수정 추가

--- src/main.js
+++ src/main.js
@@ -5,10 +5,9 @@ import App from "./App.vue";
 import router from "@/router";
 import axios from "./utils/axios";
 import store from "./vuex/store";
-import { BootstrapVue } from "bootstrap-vue-next";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 const app = createApp(App);
 app.config.globalProperties.$axios = axios; //전역변수로 설정 컴포넌트에서 this.$axios 호출할 수 있음
 app.config.globalProperties.$serverUrl = "//localhost:8081"; // api server
-app.use(router).use(store).use(BootstrapVue).mount("#app");
+app.use(router).use(store).mount("#app");
--- src/views/board/Comment.vue
+++ src/views/board/Comment.vue
@@ -29,11 +29,9 @@
     <div class="comment-list">
       <h3>Comments</h3>
       <ul>
-        <ol v-for="comment in comments" :key="comment.id" class="comment-item">
+        <li v-for="comment in comments" :key="comment.id" class="comment-item">
           <div class="comment-top">
-            <span>
-              {{ comment.author }}
-            </span>
+            <span>{{ comment.author }}</span>
             <span>
               <span class="comment-date">{{ comment.created_at }}</span>
               <span v-if="isCurrentUser(comment.author)">
@@ -45,12 +43,56 @@
                   Delete</i
                 >
                 |
-                <i class="bi bi-pencil-square edit-icon"> Edit</i>
+                <i
+                  class="bi bi-pencil-square edit-icon"
+                  @click="toggleEditForm(comment.id)"
+                >
+                  Edit</i
+                >
               </span>
             </span>
           </div>
           <p class="comment-content">{{ comment.content }}</p>
-        </ol>
+          <div class="edit-comment">
+            <div
+              class="edit-comment-container"
+              v-if="editCommentId === comment.id"
+            >
+              <div class="edit-comment-info">
+                <div class="edit-comment-user-info">
+                  <span class="edit-comment-title">댓글 수정&nbsp;&nbsp;</span>
+                  <input
+                    class="edit-comment-user-input"
+                    type="text"
+                    :value="comment.author"
+                    disabled
+                  />
+                </div>
+              </div>
+              <div class="edit-comment-textarea-wrapper">
+                <textarea
+                  class="edit-comment-textarea"
+                  v-model="editCommentContent"
+                  placeholder="수정할 내용을 입력해주세요."
+                  maxlength="8000"
+                  required
+                  tabindex="108"
+                  style="height: 99.5px"
+                ></textarea>
+                <div class="edit-comment-submit-button-wrapper">
+                  <button
+                    type="submit"
+                    class="edit-comment-submit-button"
+                    tabindex="109"
+                    @click="submitEditComment(comment.id, comment.author)"
+                  >
+                    작성
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </li>
       </ul>
     </div>
   </div>
@@ -76,6 +118,8 @@ export default {
         board_id: this.board_id, // Will be set to the current board's ID
       },
       canSubmit: false, // 댓글 입력 여부
+      editCommentId: null, // 현재 수정 중인 댓글의 ID
+      editCommentContent: "", // 수정 중인 댓글의 내용
     };
   },
   computed: {
@@ -159,9 +203,52 @@ export default {
           console.error("댓글 삭제 실패 : " + err);
         });
     },
-    goToLogin() {
-      this.$router.push("/login");
+    toggleEditForm(commentId) {
+      if (this.editCommentId === commentId) {
+        // 이미 수정 중인 댓글을 다시 클릭하면 취소
+        this.cancelEdit();
+      } else {
+        // 수정할 댓글을 클릭하면 해당 댓글의 ID와 내용을 설정
+        this.editCommentId = commentId;
+        const comment = this.comments.find((c) => c.id === commentId);
+        this.editCommentContent = comment.content;
+      }
     },
+    cancelEdit() {
+      // 수정 취소
+      this.editCommentId = null;
+      this.editCommentContent = "";
+    },
+    submitEditComment(commentId, author) {
+      if (!this.editCommentContent.trim()) {
+        alert("댓글 내용을 입력해주세요.");
+        return;
+      }
+
+      // 수정된 댓글을 서버에 저장하는 로직
+      const updatedComment = {
+        id: commentId,
+        author: author,
+        content: this.editCommentContent,
+      };
+
+      // 서버에 PATCH 요청 보내기
+      this.$axios
+        .patch(`${this.$serverUrl}/comment/update`, updatedComment)
+        .then(() => {
+          const index = this.comments.findIndex((c) => c.id === commentId);
+          if (index !== -1) {
+            this.comments[index].content = this.editCommentContent;
+          }
+          this.cancelEdit(); // 수정 완료 후 폼 숨기기
+        })
+        .catch((error) => {
+          console.error("댓글 수정 실패:", error);
+        });
+    },
+  },
+  goToLogin() {
+    this.$router.push("/login");
   },
 };
 </script>
@@ -225,6 +312,85 @@ export default {
 .comment-content {
   padding-left: 8px;
   margin: 5px 0;
+}
+
+.edit-comment {
+  margin: 1em 0 1em;
+}
+
+.edit-comment-container {
+  position: relative;
+  border: 1px solid;
+  border-color: #bbb;
+  border-radius: 5px;
+}
+
+.edit-comment-info {
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 1px solid;
+  border-color: #bbb;
+  overflow: hidden;
+}
+
+.edit-comment-user-info {
+  display: flex;
+  padding: 0.5em;
+  flex-grow: 1;
+  overflow: hidden;
+}
+
+.edit-comment-title {
+  display: block;
+  font-size: 0.9em;
+  user-select: none;
+}
+
+.edit-comment-user-input {
+  opacity: 0.5;
+  user-select: none;
+  border: none;
+}
+
+.edit-comment-textarea-wrapper {
+  position: relative;
+  padding: 0.5em 0.5em 0;
+  display: block;
+}
+
+.edit-comment-textarea {
+  border: none;
+  width: 100%;
+  min-height: 4em;
+  overflow: hidden;
+  resize: none;
+  background-color: transparent;
+}
+
+.edit-comment-textarea:focus {
+  outline: none;
+}
+
+.edit-comment-submit-button-wrapper {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  justify-content: flex-end;
+  padding-bottom: 0.5em;
+  padding-right: 0.5em;
+}
+
+.edit-comment-submit-button {
+  padding: 0.25em 1.5em;
+  border: 1px solid;
+  border-color: #bbb;
+  background-color: #fff;
+  border-radius: 4px;
+  transition-duration: 0.3s;
+}
+
+.edit-comment-submit-button:hover {
+  background-color: #eee;
 }
 
 .comment-date {
