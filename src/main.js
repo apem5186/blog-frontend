49d9e19d655c5298dc7802419b914e4d12a4f906
apem5186 <apem5186@naver.com>
2024-09-05 02:04:01 +0900

    배포용 엔드포인트 수정

--- src/main.js
+++ src/main.js
@@ -9,5 +9,7 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 
 const app = createApp(App);
 app.config.globalProperties.$axios = axios; //전역변수로 설정 컴포넌트에서 this.$axios 호출할 수 있음
-app.config.globalProperties.$serverUrl = "//localhost:8081"; // api server
+// app.config.globalProperties.$serverUrl = "//localhost:8081"; // api server
+app.config.globalProperties.$serverUrl =
+  "//ec2-43-201-145-111.ap-northeast-2.compute.amazonaws.com:8081"; // ec2 server 배포할 땐 이걸로 해야함
 app.use(router).use(store).mount("#app");
